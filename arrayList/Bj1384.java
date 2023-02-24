2cd6feb3e2ead3b1afd8f53104537854c953b502
ogayong <space8237@naver.com>
2023-02-24 09:34:52 +0900

    0224 done

--- arrayList/Bj1384.java
+++ arrayList/Bj1384.java
@@ -1,10 +1,10 @@
-package src;
+package arrayList;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Bj1384 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int index = 1;
