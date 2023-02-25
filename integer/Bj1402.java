5e68d948ba824c04ede82dca08c31eed5e3a01d6
ogayong <space8237@naver.com>
2023-02-25 14:18:00 +0900

    0225 done

--- integer/Bj1402.java
+++ integer/Bj1402.java
@@ -1,10 +1,10 @@
-package src;
+package integer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Bj1402 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
