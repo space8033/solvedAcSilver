ef435e154254c296f5fe2aa6d05f0bd8c09dc563
space8033 <space8237@naver.com>
2023-02-28 13:33:05 +0900

    0228 done

--- array/Bj1427.java
+++ array/Bj1427.java
@@ -1,13 +1,19 @@
-package src;
+package array;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class Bj1427 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
 
+        Arrays.sort(arr);
+
+        for(int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
     }
 }
