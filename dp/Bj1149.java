65973747006ff3771a3eed97aeffcf2638c7e8f0
ogayong <space8237@naver.com>
2023-03-09 14:08:26 +0900

    0309 done

---
+++ dp/Bj1149.java
@@ -0,0 +1,32 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj1149 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int[][] cost = new int[n][3];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i < n; i++) {
+            cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
+            cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
+            cost[i][2] += Math.min(cost[i - 1][0], cost[i - 1][1]);
+        }
+
+        System.out.println(Math.min(Math.min(cost[n - 1][0], cost[n -1][1]), cost[n - 1][2]));
+    }
+}
