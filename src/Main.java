081b41625715e9e27fdcca72062ae928a292a792
ogayong <space8237@naver.com>
2023-04-09 14:53:05 +0900

    0409 done

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
     }
 }
