ce85511145937cfd1663f45a0a638824d3ecd73f
space8033 <space8237@naver.com>
2023-04-11 15:11:17 +0900

    0411 done

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     }
 }
