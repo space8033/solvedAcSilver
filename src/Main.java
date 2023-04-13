8545b9f5037b6cd00228422a76da8767fb8e661d
ogayong <space8237@naver.com>
2023-04-13 13:32:42 +0900

    0413 done

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
